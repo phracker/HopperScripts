c9296db503cec3097e717c9c746b2b7791eead21
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2014-02-13 17:45:40 +0100

    Update Make JMP unconditional.py

--- Make JMP unconditional.py
+++ Make JMP unconditional.py
@@ -1,18 +1,28 @@
 doc = Document.getCurrentDocument()
 seg = doc.getCurrentSegment()
 adr = doc.getCurrentAddress()
-b = seg.readByte(adr)
-if b < 0x70 or b > 0x7F:
-	if b == 0x0F:
-		b = seg.readByte(adr + 1)
-		if b < 0x80 or b > 0x8F:
-			print "Not a conditional jump"
-		else:
-			seg.writeByte(adr, 0x90)
-			seg.writeByte(adr + 1, 0xE9)
-			seg.markAsCode(adr)
-	else:
-		print "Not a conditional jump"
+ins = seg.getInstructionAtAddress(adr)
+arch = ins.getArchitecture()
+
+if not arch in [1, 2]:
+	doc.log('Unsupported arch!')
 else:
-	seg.writeByte(adr, 0xEB)
-	seg.markAsCode(adr)
+	if not ins.isAConditionalJump():
+		doc.log('Not a conditional jump!')
+	else:
+		b = seg.readByte(adr)
+		if 0x70 <= b <= 0x7F:
+			# rel8
+			seg.writeByte(adr, 0xEB)
+			seg.markAsCode(adr)
+		elif b == 0x0F:
+			b = seg.readByte(adr + 1)
+			if 0x80 <= b <= 0x8F:
+				# rel16/32
+				seg.writeByte(adr, 0x90)
+				seg.writeByte(adr + 1, 0xE9)
+				seg.markAsCode(adr)
+			else:
+				doc.log('Unknown conditional jump!')
+		else:
+			doc.log('Unknown conditional jump!')
