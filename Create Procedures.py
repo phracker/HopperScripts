b5cbb89371d9b9f28d128ea77e94862f181d5b4c
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-04-28 14:52:13 -0700

    Detect another push ebp / mov ebp,esp style
    0x55 0x8B 0xEC is also valid. only tested on ia32

--- Create Procedures.py
+++ Create Procedures.py
@@ -18,7 +18,13 @@ while addr < last:
     # Look for the "push ebp / mov ebp, esp" pattern
     if doc.is64Bits() and seg.readByte(addr) == 0x55 and seg.readByte(addr + 1) == 0x48 and seg.readByte(addr + 2) == 0x89 and seg.readByte(addr + 3) == 0xE5:
         seg.markAsProcedure(addr)
-    if not doc.is64Bits() and seg.readByte(addr) == 0x55 and seg.readByte(addr + 1) == 0x89 and seg.readByte(addr + 2) == 0xE5:
+	addr += 3
+
+    x = seg.readByte(addr)
+    y = seg.readByte(addr + 1)
+    z = seg.readByte(addr + 2)
+    if not doc.is64Bits() and x == 0x55 and y in (0x89, 0x8B) and z in (0xE5, 0xEC):
         seg.markAsProcedure(addr)
+	addr += 2
 
     addr = addr + 1
